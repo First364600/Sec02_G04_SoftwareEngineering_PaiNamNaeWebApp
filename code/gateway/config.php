41ab8eb4e30e38262adfd72903508095e37f28ae
First364600 <sakdichai1312547@outlook.com>
2026-02-17 01:23:13 +0700

    refactor file project

---
+++ code/gateway/config.php
@@ -0,0 +1,12 @@
+<?php
+if (!defined('ACCESS_ALLOWED')) {
+    die('Direct access not permitted');
+}
+
+return [
+    'APP_ENV'       => 'production', 
+    'API_SECRET'    => 'ks4vGSTuQNGkAYieTXfTYvXVrCBffu0l',
+    'BACKEND_BASE'  => 'https://ross-possibilities-favour-airport.trycloudflare.com',
+    'RATE_LIMIT'    => 1000,
+];
+?>
